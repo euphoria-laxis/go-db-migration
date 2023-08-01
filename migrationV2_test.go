5434e893698609a0b214209cf73813d25a3481c2
euphoria-laxis <gaetangomezescanez@tutanota.com>
2023-08-01 10:47:26 +0200

    Add new migrator test

---
+++ migrationV2_test.go
@@ -0,0 +1,60 @@
+package go_db_migration
+
+import (
+	"database/sql"
+	"github.com/euphoria-laxis/go-db-migration/v2/migration"
+	"github.com/go-sql-driver/mysql"
+	"testing"
+	"time"
+)
+
+func TestGenerateMigrationV2(t *testing.T) {
+	type model1 struct {
+		ID        int          `json:"id" migration:"constraints:primary key,not null,unique,auto_increment;index"`
+		Username  string       `json:"username" migration:"constraints:not null,unique;index"`
+		CreatedAt time.Time    `json:"created_at" migration:"default:now()"`
+		UpdatedAt time.Time    `json:"updated_at" migration:"default:now()"`
+		DeletedAt sql.NullTime `json:"deleted_at" migration:"default:now()"`
+		Name      string       `json:"name" migration:"constraint:not null"`
+		Content   string       `json:"content" migration:"type:text;constraints:not null"`
+		Role      string       `json:"role" migration:"constraints:not null;default:user"`
+	}
+	type Model2 struct {
+		ID        int          `json:"id" migration:"constraints:primary key,not null,unique,auto_increment;index"`
+		Username  string       `json:"username" migration:"constraints:not null,unique;index"`
+		CreatedAt time.Time    `json:"created_at" migration:"default:now()"`
+		UpdatedAt time.Time    `json:"updated_at" migration:"default:now()"`
+		DeletedAt sql.NullTime `json:"deleted_at" migration:"default:now()"`
+		Name      string       `json:"name" migration:"constraints:not null"`
+		Content   string       `json:"content" migration:"type:text;constraints:not null"`
+		Role      string       `json:"role" migration:"constraints:not null;default:user"`
+		Valid     bool         `json:"valid" migration:"default:false"`
+	}
+	m1 := model1{}
+	m2 := Model2{}
+	cfg := mysql.Config{
+		User:                 "migration_test",
+		Passwd:               "password@123",
+		Net:                  "tcp",
+		Addr:                 "127.0.0.1:3306",
+		DBName:               "migration",
+		AllowNativePasswords: true,
+	}
+	var err error
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		t.Fatal(err)
+	}
+	pingErr := db.Ping()
+	if pingErr != nil {
+		t.Fatal(pingErr)
+	}
+	migrator := migration.NewMigrator(
+		migration.SetDB(db),
+		migration.SetDBName("migrations"),
+	)
+	err = migrator.MigrateModels(m1, m2)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
