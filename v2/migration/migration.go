8ae4a64a321f02e46f563015f193e64a6c73e105
euphoria-laxis <gaetangomezescanez@tutanota.com>
2023-08-04 20:52:38 +0200

    Add drivers for MySQL, Postgres and SQLite
            Only MySQL is available for now

--- v2/migration/migration.go
+++ v2/migration/migration.go
@@ -1,8 +1,6 @@
 package migration
 
 import (
-	"database/sql"
-	"errors"
 	"fmt"
 	"reflect"
 	"regexp"
@@ -14,142 +12,8 @@ func toSnakeCase(pattern string) string {
 	matchAllCap := regexp.MustCompile("([a-z0-9])([A-Z])")
 	snake := matchFirstCap.ReplaceAllString(pattern, "${1}_${2}")
 	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
+
 	return strings.ToLower(snake)
-}
-
-type Statistic struct {
-	NonUnique bool
-	IndexName string
-	Nullable  bool
-}
-
-func (m *Migrator) generateColumnMigration(table string, params map[string]string) error {
-	infos, err := m.getSchemaInformation(table, params["column"])
-	if err != nil && !errors.Is(err, sql.ErrNoRows) {
-		return err
-	}
-	if infos != nil && convertSqlDataType(infos.Type) != strings.ToLower(params["type"]) {
-		query := fmt.Sprintf(
-			"ALTER TABLE %s DROP COLUMN %s;",
-			table,
-			params["column"],
-		)
-		_, err = m.DB.Exec(query)
-		if err != nil {
-			return err
-		}
-	}
-	query := fmt.Sprintf(
-		"ALTER TABLE %s ADD COLUMN %s %s;\n",
-		table,
-		params["column"],
-		params["type"],
-	)
-	_, err = m.DB.Exec(query)
-	if err != nil {
-		rowExist := strings.Contains(err.Error(), "Duplicate column name")
-		if !rowExist {
-			return err
-		}
-	}
-	constraints, hasConstraint := params["constraints"]
-	if hasConstraint {
-		for _, constraint := range strings.Split(constraints, ",") {
-			if !checkConstraint(constraint) {
-				fmt.Printf("[WARN] constraint %s is not valid and was ignored\n", constraint)
-				continue
-			}
-			if infos != nil {
-				if constraint == "not null" && infos.Null == "YES" {
-					continue
-				} else if constraint == "unique" && infos.Key == "UNI" {
-					continue
-				}
-			}
-			query = fmt.Sprintf(
-				"ALTER TABLE %s ",
-				table,
-			)
-			query += fmt.Sprintf(
-				"MODIFY %s %s %s;\n",
-				params["column"],
-				params["type"],
-				constraint,
-			)
-			_, err = m.DB.Exec(query)
-			if err != nil {
-				return err
-			}
-		}
-	}
-	defaultValue, hasDefaultValue := params["default"]
-	if hasDefaultValue {
-		if strings.Contains(params["type"], "VARCHAR") {
-			defaultValue = "'" + defaultValue + "'"
-		}
-		query = fmt.Sprintf(
-			"ALTER TABLE %s ALTER %s SET DEFAULT %s;\n",
-			table,
-			params["column"],
-			defaultValue,
-		)
-
-		_, err = m.DB.Exec(query)
-		if err != nil {
-			return err
-		}
-	}
-	_, isIndex := params["index"]
-	if isIndex {
-		query = fmt.Sprintf(
-			"SELECT NON_UNIQUE, INDEX_NAME, NULLABLE FROM information_schema.statistics WHERE table_name = '%s' AND column_name = '%s';",
-			table,
-			params["column"],
-		)
-		var statistic Statistic
-		err = m.DB.QueryRow(query).Scan(&statistic.NonUnique, &statistic.IndexName, &statistic.Nullable)
-		if err != nil && !errors.Is(err, sql.ErrNoRows) {
-			return err
-		}
-		if errors.Is(err, sql.ErrNoRows) {
-			query = fmt.Sprintf(
-				"CREATE INDEX index_%s ON %s (%s);\n",
-				params["column"],
-				table,
-				params["column"],
-			)
-			_, err = m.DB.Exec(query)
-			if err != nil {
-				return err
-			}
-		}
-	}
-
-	return nil
-}
-
-type Result struct {
-	Field   string
-	Type    string
-	Null    string
-	Key     string
-	Extra   string
-	Default interface{}
-}
-
-func (m *Migrator) getSchemaInformation(table, column string) (*Result, error) {
-	query := fmt.Sprintf(
-		"select COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA, COLUMN_DEFAULT from information_schema.COLUMNS where table_name = '%s' and column_name = '%s' ;",
-		table,
-		column,
-	)
-	var result Result
-	err := m.DB.QueryRow(query).Scan(&result.Field, &result.Type, &result.Null, &result.Key, &result.Extra, &result.Default)
-	if err != nil {
-		return nil, err
-	}
-
-	return &result, err
 }
 
 func (m *Migrator) migrateModel(model reflect.Type) error {
@@ -199,9 +63,27 @@ func (m *Migrator) migrateModel(model reflect.Type) error {
 			kind := column.Type.String()
 			values["type"] = m.convertType(kind)
 		}
-		err = m.generateColumnMigration(table, values)
-		if err != nil {
-			return err
+		switch m.Driver {
+		case DBDriverMySQL:
+			err = m.generateMySqlColumnMigration(table, values)
+			if err != nil {
+				return err
+			}
+			break
+		case DBDriverPostgres:
+			err = m.generatePostgresColumnMigration(table, values)
+			if err != nil {
+				return err
+			}
+			break
+		case DBDriverSQLite:
+			err = m.generateSqliteColumnMigration(table, values)
+			if err != nil {
+				return err
+			}
+			break
+		default:
+			return fmt.Errorf("unknown driver: %v, allowed drivers: [mysql,postgres,sqlite]", m.Driver)
 		}
 	}
 
