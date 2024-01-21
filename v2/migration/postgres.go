6bd6904f0345a091c5b7a6263a1e3c92e47bf9c6
euphoria-laxis <euphoria.laxis@euphoria-laxis.com>
2024-01-22 00:37:40 +0100

    Split schema creation by driver

--- v2/migration/postgres.go
+++ v2/migration/postgres.go
@@ -1,5 +1,18 @@
 package migration
 
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+	"reflect"
+	"strings"
+)
+
+func (m *Migrator) createPostgresSchema(table string, model reflect.Type) error {
+	return nil
+}
+
 func (m *Migrator) generatePostgresColumnMigration(table string, params map[string]string) error {
 	return nil
 }
